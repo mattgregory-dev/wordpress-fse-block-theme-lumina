b82c730cd4fe24c512d6d29f57ebc097ea1c7733
mattgregory-dev <matthew@marketingstarz.com>
2026-08-01 02:32:52 -0700

    feat: add checklist-section block

--- inc/blocks.php
+++ inc/blocks.php
@@ -19,5 +19,6 @@ function lumina_register_blocks() {
 	register_block_type( get_template_directory() . '/build/spotlight' );
 	register_block_type( get_template_directory() . '/build/bio' );
 	register_block_type( get_template_directory() . '/build/cta-band' );
+	register_block_type( get_template_directory() . '/build/checklist-section' );
 }
 add_action( 'init', 'lumina_register_blocks' );
