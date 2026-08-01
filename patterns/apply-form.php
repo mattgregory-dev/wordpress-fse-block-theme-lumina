d5635cdd4e3b77c8c740509e7b2d87557c9c0983
mattgregory-dev <matthew@marketingstarz.com>
2026-08-01 13:23:50 -0700

    feat: refine heading above application form

--- patterns/apply-form.php
+++ patterns/apply-form.php
@@ -8,13 +8,13 @@
  * @package lumina-blocks
  */
 ?>
-<!-- wp:group {"tagName":"section","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained","contentSize":"720px"}} -->
-<section class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--80);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading -->
-<h2 class="wp-block-heading">Your Application</h2>
+<!-- wp:group {"tagName":"section","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|80"}}},"layout":{"type":"constrained","contentSize":"720px"}} -->
+<section class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--80)"><!-- wp:heading {"textAlign":"center"} -->
+<h2 class="wp-block-heading has-text-align-center">Your Application</h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>Everything you share is confidential. The more honest you can be, the better we can understand how to care for you.</p>
+<!-- wp:paragraph {"align":"center","className":"has-balanced-text"} -->
+<p class="has-text-align-center has-balanced-text">Everything you share is confidential. The more honest you can be, the better we can understand how to care for you.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:shortcode -->
